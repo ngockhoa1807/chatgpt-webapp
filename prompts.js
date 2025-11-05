91b79380b872337234a382676db94c95cfeecc95
ngockhoa1807 <ngockhoa1807@gmail.com>
2025-11-05 10:33:04 +0700

    Create prompts.js

---
+++ prompts.js
@@ -0,0 +1,29 @@
+export const basePrompt = "tạo bài tập xác suất thống kê trung học cơ sở, chương trình GDPT 2018";
+
+export const promptsBasic = {
+  types: {
+    tracnghiem: "tạo bài tập trắc nghiệm 4 lựa chọn",
+    tuluan: "tạo bài tập tự luận"
+  },
+  semesters: {
+    hki: "Kiến thức học kỳ 1 lớp 9",
+    hkii: "Kiến thức học kỳ 2 lớp 9"
+  },
+  levels: {
+    nhanbiet: "mức độ nhận biết",
+    thonghieu: "mức độ thông hiểu",
+    vandung: "mức độ vận dụng",
+    vandungcao: "mức độ vận dụng cao"
+  }
+};
+
+export const promptsAdvanced = {
+  format: `Trình bày kết quả theo mẫu:
+I. TRẮC NGHIỆM
+II. TỰ LUẬN
+*Đáp án, hướng dẫn làm bài`,
+  tracnghiem:
+    "tạo bài tập trắc nghiệm 4 lựa chọn, nội dung xác suất thống kê theo chương trình trung học cơ sở GDPT 2018",
+  tuluan:
+    "tạo bài tập tự luận, nội dung xác suất thống kê theo chương trình trung học cơ sở GDPT 2018"
+};
