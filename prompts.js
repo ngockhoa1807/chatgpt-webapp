53d77f3cc48cb63377f6a83437d3ee9a4494b0ad
ngockhoa1807 <ngockhoa1807@gmail.com>
2025-11-05 18:06:38 +0700

    Update prompts.js

--- prompts.js
+++ prompts.js
@@ -8,9 +8,9 @@ export const promptsBasic = {
     hkii: "Nội dung kiến thức học kỳ II lớp 9."
   },
   level: {
-    nhanbiet: "Mức độ nhận biết (câu hỏi cơ bản, kiểm tra kiến thức nền).",
-    thonghieu: "Mức độ thông hiểu (giải thích, vận dụng trực tiếp).",
-    vandung: "Mức độ vận dụng (giải bài tập trung bình - khá).",
-    vandungcao: "Mức độ vận dụng cao (bài toán tổng hợp, nâng cao)."
+    nhanbiet: "Mức độ nhận biết .",
+    thonghieu: "Mức độ thông hiểu .",
+    vandung: "Mức độ vận dụng .",
+    vandungcao: "Mức độ vận dụng cao ."
   }
 };
