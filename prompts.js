4d7e27f713d5d7248cf34e27cb5f80380302cad6
ngockhoa1807 <ngockhoa1807@gmail.com>
2025-11-05 16:19:26 +0700

    Update prompts.js

--- prompts.js
+++ prompts.js
@@ -1,9 +1,9 @@
-export const basePrompt = "tạo bài tập xác suất thống kê trung học cơ sở, chương trình GDPT 2018";
-
 export const promptsBasic = {
   types: {
-    tracnghiem: "tạo bài tập trắc nghiệm 4 lựa chọn",
-    tuluan: "tạo bài tập tự luận"
+   tracnghiem:
+    "tạo bài tập trắc nghiệm 4 lựa chọn, nội dung xác suất thống kê theo chương trình trung học cơ sở GDPT 2018",
+  tuluan:
+    "tạo bài tập tự luận, nội dung xác suất thống kê theo chương trình trung học cơ sở GDPT 2018"
   },
   semesters: {
     hki: "Kiến thức học kỳ 1 lớp 9",
